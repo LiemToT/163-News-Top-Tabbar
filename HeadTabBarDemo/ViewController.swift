fe51922d4760cac96100907a48e54521d6756962
linyi101 <804121494@qq.com>
2016-05-31 17:46:52 +0800

    Complete headTabbar

--- HeadTabBarDemo/ViewController.swift
+++ HeadTabBarDemo/ViewController.swift
@@ -9,16 +9,57 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var tabTitleArray: NSArray?
+    var headScrollView: UIScrollView?
+    var lastTapButton: UIButton?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+        
+        self.automaticallyAdjustsScrollViewInsets = false
+        
+        tabTitleArray = ["头条", "娱乐", "体育", "财经", "科技", "NBA", "手机"]
+        
+        headScrollView = UIScrollView(frame: CGRectMake(0, 64, CGRectGetWidth(view.bounds), 40))
+        headScrollView!.contentSize = CGSizeMake(CGFloat(tabTitleArray!.count * 80), 40)
+        headScrollView!.backgroundColor = UIColor.greenColor()
+        headScrollView?.showsHorizontalScrollIndicator = false
+        
+        for i in 0 ..< tabTitleArray!.count {
+            let btn = UIButton(type: .Custom)
+            btn.frame = CGRectMake(CGFloat(i * 80), 0, 80, 40)
+            btn.setTitle(tabTitleArray![i] as? String, forState: .Normal)
+            btn.tag = i + 100
+            btn.setTitleColor(UIColor.redColor(), forState: .Selected)
+            btn.titleLabel?.font = UIFont.boldSystemFontOfSize(15)
+            btn.addTarget(self, action: Selector("didClickHeadButtonAction:"), forControlEvents: .TouchUpInside)
+            headScrollView!.addSubview(btn)
+        }
 
+        view.addSubview(headScrollView!)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
 
-
+    func didClickHeadButtonAction(btn: UIButton) {
+        if btn == lastTapButton {
+            return
+        }
+        
+        btn.selected = true
+        lastTapButton?.selected = false
+        
+        UIView.animateWithDuration(0.5, delay: 0, usingSpringWithDamping: 0.3, initialSpringVelocity: 0, options: .CurveEaseIn, animations: { () -> Void in
+            btn.transform = CGAffineTransformMakeScale(1.3, 1.3)
+            self.lastTapButton?.transform = CGAffineTransformIdentity
+            }, completion: { (finish) -> Void in
+                self.lastTapButton = btn
+        })
+        
+        
+    }
 }
 
